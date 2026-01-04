8649dd4ccfc983460510add338df6bdefa1bfc21
Diego Schivo <diego.schivo@gmail.com>
2026-01-04 11:08:18 +0100

    cors

--- backend/source/com/janilla/websitetemplate/backend/CustomInvocationHandlerFactory.java
+++ backend/source/com/janilla/websitetemplate/backend/CustomInvocationHandlerFactory.java
@@ -85,9 +85,12 @@ public class CustomInvocationHandlerFactory extends InvocationHandlerFactory {
 				throw new HandleException(new MethodBlockedException());
 		}
 
-		var rs = exchange.response();
-		rs.setHeaderValue("access-control-allow-origin", configuration.getProperty("website-template.api.cors.origin"));
-		rs.setHeaderValue("access-control-allow-credentials", "true");
+		var o = configuration.getProperty("website-template.api.cors.origin");
+		if (o != null && !o.isEmpty()) {
+			var rs = exchange.response();
+			rs.setHeaderValue("access-control-allow-credentials", "true");
+			rs.setHeaderValue("access-control-allow-origin", o);
+		}
 
 //		if (r.getPath().startsWith("/api/"))
 //			try {
